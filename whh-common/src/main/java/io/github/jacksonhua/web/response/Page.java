655a83c24f130a2d46438b89dabe2753cb9beb51
asd11 <a>
2025-12-24 19:37:49 +0800

    common添加公共内容

---
+++ whh-common/src/main/java/io/github/jacksonhua/web/response/Page.java
@@ -0,0 +1,46 @@
+package io.github.jacksonhua.web.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Page<T> {
+    private int pageSize = -1;
+    private long total;
+    private int page = 0;
+    private List<T> data;
+
+    public static <T> Page<T> of(com.github.pagehelper.Page<T> result) {
+        Page<T> pageData = new Page<>();
+        pageData.setData(result.getResult());
+        pageData.setTotal(result.getTotal());
+        pageData.setPage(result.getPageNum());
+        pageData.setPageSize(result.getPageSize());
+        return pageData;
+    }
+    public static <T> Page<T> of(com.baomidou.mybatisplus.extension.plugins.pagination.Page<T> result) {
+        Page<T> pageData = new Page<>();
+        pageData.setData(result.getRecords());
+        pageData.setTotal(result.getTotal());
+        pageData.setPage(result.getCurrent());
+        pageData.setPageSize(result.getSize());
+        return pageData;
+    }
+    public static <T> Page<T> of(com.baomidou.mybatisplus.core.metadata.IPage<T> result) {
+        Page<T> pageData = new Page<>();
+        pageData.setData(result.getRecords());
+        pageData.setTotal(result.getTotal());
+        pageData.setPage(result.getCurrent());
+        pageData.setPageSize(result.getSize());
+        return pageData;
+    }
+    public static <T> Page<T> data(List<T> data) {
+        Page<T> pageData = new Page<>();
+        pageData.setData(data);
+        return pageData;
+    }
+
+
+
+}
